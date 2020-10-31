6cfc98b357b46cc2f8ef1430c6ba589432047c27
LeBron-Jian <37261042+LeBron-Jian@users.noreply.github.com>
2020-10-31 10:38:04 +0800

    Update 59_1_滑动窗口最大值.py

--- 剑指offer/59_1_滑动窗口最大值.py
+++ 剑指offer/59_1_滑动窗口最大值.py
@@ -60,7 +60,7 @@ class Solution:
         
 
 
-class Solution:
+class Solution2:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
         '''
             当 Nums 为空时如果有个 max() 则会产生空值报错问题，因此需要注意
@@ -85,6 +85,58 @@ class Solution:
         return max_list
 
 
+class Solution3:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        '''
+            滑动窗口解题思路：官方解法：单调队列
+            
+            设窗口区间为[i, j]，最大值为x_j。当窗口向前移动一格，则区间变为[i+1, j+1]，即添加了
+            nums[j+1]，删除了 nums[i]
+            
+            若只向窗口[i, j] 右边添加数字 nums[j+1]，则新窗口最大值可以通过一次对比，使用O(1)时间得到：
+            x_j+1 = max(x_j， nums[j+1])
+            
+            而由于删除的 nums[i] 可能恰好是窗口内唯一的最大值 x_j，因此不能通过上述方法计算x_j+1，而必须使用O(j-i)时间
+            遍历整个窗口区间获取最大值，即：
+            x_j+1 = max(nums(i+1), nums(j+1))
+            根据以上分析，可得暴力法的时间复杂度为O(n-k+1)k  约等于 O(nk)
+                设数组 nums 的长度为n，则共有 n-k+1 个窗口
+                获取每个窗口最大值需详细遍历，时间复杂度为O(k)
+                
+               获取窗口很简单，但是难的是：如何每次窗口滑动后，将“获取窗口内的最大值” 的时间复杂度从O(k) 降低到 O(1)
+        '''
+        from collections import deque
+        q = deque()
+        res, n = [], len(nums)
+        for i,j in zip(range(1-k, n+1-k), range(n)):
+            if i>0 and q[0] == nums[i-1]:
+                q.popleft()  # 删除 q中对应的 nums[i-1]
+            while q and q[-1] < nums[j]:
+                q.pop()  # 保持q递减
+            q.append(nums[j])
+            if i >= 0:
+                res.append(q[0])  # 记录窗口最大值
+        return res
+        
+        
+        def maxSlidingWindow(self, nums, k):
+            if not nums or k==0:
+                return []
+            deque = deque()
+            for i in range(k):  # 未形成窗口
+                while deque and deque[-1] < nums[i]:
+                    deque.pop()
+                deque.append(nums[i])
+            res = deque[0]
+            for i in range(k, len(nums)):
+                if  deque[0] == nums[i-k]:
+                    deque.popleft()
+                while deque and deque[-1] < nums[i]:
+                    deque.pop()
+                deque.append(nums[i])
+                res.append(deque[0])
+            return res
+                    
 
 re = Solution()
 print(re.maxSlidingWindow(nums= [1,3,-1,-3,5,3,6,7], k=3))
